5275a71b1f9bc567900d1f655965378218260bf7
vilacolab <vilacolab@gmail.com>
2026-06-15 18:46:02 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Bem-Vindo 🍎',
+  title: 'VilaColab | Bem-vindo 🍎',
   description:
     'Responda algumas perguntas para que possamos entender melhor o seu negócio e como podemos ajudar.',
   robots: { index: false, follow: false },
